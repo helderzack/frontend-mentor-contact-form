872426033ad87c9d38e33985329dcfa1fe97d7dc
helderzack <helderzacarias@outlook.com>
2024-06-03 09:28:12 -0300

    Designed with a mobile-first approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contact-form';
+  contactForm = new FormGroup(
+    {
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      emailAddress: new FormControl(''),
+      queryType: new FormControl(''),
+      message: new FormControl(''),
+      consent: new FormControl('')
+    }
+  );
+
+  onSubmit() {
+    console.warn(this.contactForm.value);
+  }
 }
